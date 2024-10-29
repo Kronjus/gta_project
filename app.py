3a6613306ec7fd1d4a0812f145888389e1bb5967
jujansen <jujansen@student.ethz.ch>
2024-10-29 13:50:47 +0100

    Finally figured vercel.json out...

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():  # put application's code here
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run()
